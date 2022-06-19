4a5755701f2e44b2f4d40a3539a6e58d3f0772de
DelisyaJayadi <93379700+DelisyaJayadi@users.noreply.github.com>
2022-06-19 22:41:34 +0700

    PraktikumPBO13

---
+++ Healer.java
@@ -0,0 +1,16 @@
+package PraktikumPBO12;
+
+public class Healer extends Character {
+    public Healer() {
+        super(70, 10, 10);
+    }
+
+    @Override
+    public boolean attack() {
+        return 1 <= getRandomNumber() && getRandomNumber() <= (0.85 * 10);
+    }
+
+    public void heal() {
+        setHP(getHP() + 25);
+    }
+}
